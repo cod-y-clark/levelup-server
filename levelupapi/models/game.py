b74f547046272ea6763548a06de10ccbe912dd4d
cod-y-clark <cody66clark@gmail.com>
2022-11-19 13:02:03 -0600

    Updating fixtures

--- levelupapi/models/game.py
+++ levelupapi/models/game.py
@@ -10,4 +10,4 @@ class Game(models.Model):
     maker = models.CharField(max_length=50)
     gamer = models.ForeignKey(Gamer, on_delete=models.CASCADE)
     number_of_players = models.IntegerField()
-    skill_level = models.IntegerField(max_length=1)
+    skill_level = models.IntegerField()
