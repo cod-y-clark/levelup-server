cb9534570ed16349610574589909e4bdf6b32061
cod-y-clark <cody66clark@gmail.com>
2022-12-10 08:50:52 -0600

    finishing up

--- levelup/urls.py
+++ levelup/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
+from levelupapi.views import register_user, check_user
 from django.conf.urls import include
 from rest_framework import routers
-from levelupapi.views import register_user, check_user, GameTypeView, EventView, GameView
+from levelupapi.views import GameTypeView, EventView, GameView
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'gametypes', GameTypeView, 'gametype')
